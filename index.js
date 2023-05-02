912b229e4dde059bf580af6a871c5d37add0e480
Ty Blackwell <tyeverettblackwell@gmail.com>
2023-05-02 11:13:19 -0400

    pass test

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// employee object
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St",
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, ...{ [key]: value } }
+  };
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  };
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newObj = { ...employee }
+    delete newObj[key];
+    return newObj;
+  };
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  };
